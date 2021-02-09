1451db756d222c927988252d1ec962b6b6c2fbdb
Hassan Aftab Mughal <hassanaftabmughal@gmail.com>
2021-02-09 14:10:24 +0500

    Basic Data Types

---
+++ PythonProgrammingBasics/BasicDataTypes.py
@@ -0,0 +1,18 @@
+print("*****************")
+print("Basic Data Types")
+print("*****************\n\n")
+
+
+an_int = 10
+a_float = 10.5
+a_string = 'Hassan Aftab'
+a_list = [an_int, a_float, a_string]
+
+print("Integer :", str(an_int))
+print("Float :", str(a_float))
+print("String :", a_string)
+print("List :", a_list)
+
+print("\n\n--------------")
+print("Program Ended")
+print("--------------")
